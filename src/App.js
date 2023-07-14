5d782a59770f8363c01db59cb09bfd811417adc2
unknown <edgar.rivadeneira@gmail.com>
2023-07-14 16:11:58 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          ARCOMRED 
+        </h1>
+        <h3>
+          Soluciones Integrales a Domicilio
+        </h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
